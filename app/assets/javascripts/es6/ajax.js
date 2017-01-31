7eb396059bb3b674b64adf66985816cd05d71011
Yuri Brito <yuriabrito@gmail.com>
2017-01-30 22:48:11 -0300

    UX live updates to analytics

--- app/assets/javascripts/es6/ajax.js
+++ app/assets/javascripts/es6/ajax.js
@@ -7,14 +7,28 @@ const ajaxAction = (query, action) => {
   });
 }
 
+const sumToQueryCount = (query, value) => {
+  const query_count = $(`#query-${query.search}`).find('query-count');
+  const count = parseInt(query_count.html());
+
+  if(count + value == 0) $(`#query-${query.search}`).remove();
+  else query_count.html(count + value);
+
+  return query_count;
+}
+
 export const ajaxIncrement = query => {
   ajaxAction(query, 'increment').done(response => {
-    console.log(response);
+    if(sumToQueryCount(query, 1).length > 0) return;
+    $('queries').append(`<query id="query-${query.search}">
+                        <query-search>${query.search}</query-search>
+                        <query-count>1</query-count>
+                        </query>`);
   });
 }
 
 export const ajaxDecrement = query => {
   ajaxAction(query, 'decrement').done(response => {
-    console.log(response);
+    sumToQueryCount(query, -1);
   });
 }
