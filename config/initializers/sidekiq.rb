ae4afa1c5b8c8296a6b9a05e2b6a783f6e7c1367
Yuri Brito <yuriabrito@gmail.com>
2017-01-31 01:09:29 -0300

    ENV REDIS

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,10 +2,10 @@ require 'sidekiq'
 
 if Rails.env.production?
   Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDIS_URL'] }
+    config.redis = { url: ENV['REDIS_PROVIDER'] }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: ENV['REDIS_URL'] }
+    config.redis = { url: ENV['REDIS_PROVIDER'] }
   end
 end
